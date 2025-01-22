8c7680528663b9855cb030a4ed49ea93ea1f2702
Matej1903 <martic.matej1903@gmail.com>
2025-01-22 11:28:40 +0100

    Add files via upload

---
+++ python.igra.py
@@ -0,0 +1,20 @@
+import random
+
+tajnibroj = random.randint(1,30)
+
+pogodi = 0
+
+for x in range (5):
+
+    pogodi = int(input("Pogodi jedan broj između 1 i 30: "))
+
+    if pogodi == tajnibroj:
+        print("Bravo, pogodio si broj.")
+        break
+    elif pogodi > tajnibroj:
+        print("Fulao si, probaj neki manji broj.")
+    elif pogodi < tajnibroj:
+        print("Fulao s, probaj neki veći broj.")
+
+print("Kraj programa...")
+print("Traženi broj u ovoj igri bio je: " + str(tajnibroj))
